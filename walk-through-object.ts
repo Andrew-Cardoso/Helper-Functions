77b385db98a0c3a0e5947c0557ed7e7063dd8a03
Andrew-Cardoso <62756962+Andrew-Cardoso@users.noreply.github.com>
2021-08-18 12:11:24 -0300

    Update walk-through-object.ts

--- walk-through-object.ts
+++ walk-through-object.ts
@@ -1,3 +1,28 @@
+/*
+Usage:
+const obj = {
+	arr1: [
+		{
+			innerArr: [ 0, 1, 2, 3 ]
+		}
+	],
+	arr2: [
+		{
+			innerArr: [ 4, 5, 6, 7 ]
+		}
+	],
+	arr3: [
+		{
+			innerArr: [ 8, 9, 'A', 'B' ]
+		},
+		{
+			innerArr: [ 'C', 'D', 'E', 'F', { key: 42 } ]
+		}
+	],
+};
+console.log( walkThroughObject(obj, 'arr3.innerArr.key') );
+*/
+
 const walk = <T> ( result: any, steps: string[] ): T => {
 	if ( steps.length === 0 ) return <T> result;
 	const step = steps.shift()!;
