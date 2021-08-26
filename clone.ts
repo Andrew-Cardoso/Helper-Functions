19281eeb5b5794809304ed3b13989996be9f199d
Andrew-Cardoso <62756962+Andrew-Cardoso@users.noreply.github.com>
2021-08-26 01:29:06 -0300

    Update clone.ts

--- clone.ts
+++ clone.ts
@@ -2,6 +2,9 @@ import { isPrimitiveType } from "./is-primitive-type";
 
 export const clone = <T> ( value: T ): T => {
 	if ( !value || isPrimitiveType( value ) ) return value;
+	
+	if ( Object.prototype.toString.call( value ) === '[object Date]' )
+		return new Date( value as unknown as Date ) as unknown as T;
 
 	if ( Array.isArray( value ) ) return <T> <unknown> value.map( clone );
 
